a4705cb573d595f81f78522f2312c66c565e0bd8
Muhammad Afiq <blackzafiqz@gmail.com>
2026-02-06 04:53:36 +0800

    feat: include all responce from acme server

--- libacme/Models/Response/Meta.cs
+++ libacme/Models/Response/Meta.cs
@@ -3,4 +3,7 @@ namespace LibAcme.Models.Response;
 public record Meta
 {
     public string? TermsOfService { get; init; }
+    public string? Website { get; init; }
+    public List<string>? CaaIdentities { get; init; }
+    public bool? ExternalAccountRequired { get; init; }
 }
