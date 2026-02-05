89cd126622b49966ae1d1cee6bd22c530a02817c
Muhammad Afiq <blackzafiqz@gmail.com>
2026-02-06 04:31:47 +0800

    fix: wrong dir naming

---
+++ LibAcme/AcmeClient.cs
@@ -0,0 +1,48 @@
+﻿using System.Net.Http.Json;
+using LibAcme.Models;
+using Microsoft.Extensions.Logging;
+using Directory = LibAcme.Models.Response.Directory;
+
+namespace LibAcme;
+
+public class AcmeClient
+{
+    private readonly Config _config;
+    private readonly HttpClient _httpClient;
+    private readonly ILogger _logger;
+    public AcmeClient(Config config)
+    {
+        _config = config;
+        _httpClient = new HttpClient();
+        using ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole());
+        _logger = factory.CreateLogger<AcmeClient>();
+    }
+
+    public AcmeClient(Config config, HttpClient httpClient)
+    {
+        _config = config;
+        _httpClient = httpClient;
+        using ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole());
+        _logger = factory.CreateLogger<AcmeClient>();
+    }
+    public AcmeClient(Config config,HttpClient httpClient, ILogger logger)
+    {
+        _config = config;
+        _httpClient = httpClient;
+        _logger = logger;
+    }
+    private async Task<Directory> GetDirectory()
+    {
+        return await _httpClient.GetFromJsonAsync<Directory>(_config.AcmeDirectoryUrl,Utils.JsonOptions) 
+               ?? throw new InvalidOperationException("Either your provided AcmeDirectoryUrl is invalid or the ACME server is not responding correctly.");
+    }
+    public async Task <string?> GetDirectoryTnc()
+    {
+        var dir = await GetDirectory();
+        if (dir.Meta?.TermsOfService is null)
+        {
+            return null;
+        }
+        return dir.Meta.TermsOfService;
+    }
+}
--- libacme/Acme.cs
+++ libacme/Acme.cs
@@ -1,44 +1,10 @@
-﻿using System.Net.Http.Json;
-using LibAcme.Models;
-using Microsoft.Extensions.Logging;
-using Directory = LibAcme.Models.Response.Directory;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-namespace LibAcme;
-
-public class Acme
+namespace LibAcme
 {
-    private readonly Config _config;
-    private readonly HttpClient _httpClient;
-    private readonly ILogger _logger;
-    private readonly Directory _directory;
-    public Acme(Config config)
+    internal class Acme
     {
-        _config = config;
-        _httpClient = new HttpClient();
-        using ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole());
-        _logger = factory.CreateLogger<Acme>();
-    }
-
-    public Acme(Config config, HttpClient httpClient)
-    {
-        _config = config;
-        _httpClient = httpClient;
-        using ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole());
-        _logger = factory.CreateLogger<Acme>();
-    }
-    public Acme(Config config,HttpClient httpClient, ILogger logger)
-    {
-        _config = config;
-        _httpClient = httpClient;
-        _logger = logger;
-    }
-    private async Task<Directory> GetDirectory()
-    {
-        return await _httpClient.GetFromJsonAsync<Directory>(_config.AcmeDirectoryUrl,Utils.JsonOptions) 
-               ?? throw new InvalidOperationException("Either your provided AcmeDirectoryUrl is invalid or the ACME server is not responding correctly.");
-    }
-    public Task <string> GetDirectoryTnc()
-    {
-        _httpClient.GetAsync()
     }
 }
